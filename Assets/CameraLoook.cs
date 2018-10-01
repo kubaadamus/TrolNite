f07ed607e634254b450443864b4d48c1db648508
kubaadamus <jakubadamus1991@gmail.com>
2018-10-01 20:15:53 +0200

    Scena wyczyszczona z pierwszego playera bazującego na Rigidbody. Teraz posługuję się wyłącznie CharacterControlerem

--- Assets/CameraLoook.cs
+++ Assets/CameraLoook.cs
@@ -11,12 +11,16 @@ public class CameraLoook : MonoBehaviour {
     private float rotY = 0.0f; // rotation around the up/y axis
     private float rotX = 0.0f; // rotation around the right/x axisn
     public GameObject character;
+    SpringJoint Joint = null;
+    Rigidbody targetJoint;
+    float Distance = 2;
     void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Raycast();
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = -Input.GetAxis("Mouse Y");
         rotY += mouseX * mouseSensitivity * Time.deltaTime;
@@ -25,5 +29,50 @@ public class CameraLoook : MonoBehaviour {
         Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
         character.transform.rotation = Quaternion.Euler(character.transform.rotation.x, rotY, character.transform.rotation.z);
         transform.rotation = localRotation;
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            if (Joint != null)
+            {
+                Destroy(Joint);
+                Joint = null;
+            }
+        }
+    }
+    void Raycast()
+    {
+
+        RaycastHit hit;
+        Ray landingRay = new Ray(transform.position,transform.forward);
+        Debug.DrawRay(transform.position,transform.forward * Distance);
+
+        //STRZELANIE RAYCASTEM
+        if (Physics.Raycast(landingRay, out hit, Distance))
+        {
+            if (hit.collider.tag == "gripable" && Input.GetMouseButtonDown(0))
+            {
+                if (Joint == null)
+                {
+                    Joint = gameObject.AddComponent(typeof(SpringJoint)) as SpringJoint;
+                    Joint.anchor = new Vector3(0, 0, 0);
+                    Joint.autoConfigureConnectedAnchor = false;
+                    Joint.connectedAnchor = new Vector3(0, 0, 0);
+                    Joint.minDistance = 1.0f;
+                    Joint.maxDistance = 2.0f;
+                    Debug.Log("GRIP XD");
+                    targetJoint = hit.rigidbody;
+                    Joint.connectedBody = targetJoint;
+                    Joint.enableCollision = true;
+                    GetComponent<Rigidbody>().isKinematic = true;
+                    GetComponent<Rigidbody>().useGravity = false;
+
+                }
+
+
+            }
+            if (hit.collider.tag == "")
+            {
+            }
+        }
     }
 }
